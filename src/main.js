f3b965e13dfe400d7d3b9860bd4ac324de4abedb
Bdld <790899882@qq.com>
2023-06-11 15:29:38 +0800

    搭建了Layout静态页面

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+// 引入初始化样式文件
+import "@/styles/common.scss";
 
-import App from './App.vue'
-import router from './router'
+// 测试接口函数
+import { getCategory } from "@/apis/testAPI";
+getCategory().then(res => {
+  console.log(res);
+});
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
---
+++ src/views/Category/index.vue
@@ -0,0 +1,11 @@
+<template>
+  category
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
